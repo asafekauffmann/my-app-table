3fd5972653ad740723c1d6ef64a221be345d9af0
Asafe Kauffmann <asafe.kauffmann@gmail.com>
2023-05-08 18:19:41 -0300

    Up second model

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-//import logo from './logo.svg';
 import './App.css';
+
 
 const people = [
   { 
@@ -49,49 +49,88 @@ const people = [
 ]
 
 
+// Array Second model
+const peoples = [
+  {
+    name: 'Leslie Alexander',
+    email: 'leslie.alexander@example.com',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      ' ',
+    href: '#',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Michael Foster',
+    email: 'michael.foster@example.com',
+    role: 'Co-Founder / CTO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    href: '#',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Dries Vincent',
+    email: 'dries.vincent@example.com',
+    role: 'Business Relations',
+    imageUrl:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    href: '#',
+    lastSeen: null,
+  },
+  {
+    name: 'Lindsay Walton',
+    email: 'lindsay.walton@example.com',
+    role: 'Front-end Developer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    href: '#',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Courtney Henry',
+    email: 'courtney.henry@example.com',
+    role: 'Designer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    href: '#',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Tom Cook',
+    email: 'tom.cook@example.com',
+    role: 'Director of Product',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    href: '#',
+    lastSeen: null,
+  },
+]
+
 function App() {
   return (
-    
-    <div className="px-4 sm:px-6 lg:px-9">
+    // First model 
+    /* <div className="px-4 sm:px-6 lg:px-9">
       <div className="mt-5 flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-7">
           <div className="inline-block min-w-full py-1 mx-2 align-middle">
             <table className="min-w-full divide-y divide-gray-300">
               <thead>
                 <tr>
-                <th scope="col" className="px-3 py-3 text-left">
-                    Status
-                  </th>
-                  <th scope="col" className="px-3 py-3 text-left">
-                    ID 
-                  </th>
-                  <th scope="col" className="px-3 py-3 text-left">
-                    Envelop ID
-                  </th>
-                  <th scope="col" className="px-3 py-3 text-left">
-                    Created At
-                  </th>
-                  <th scope="col" className="px-3 py-3 text-left">
-                    Updated At
-                  </th>
-                  <th scope="col" className="px-3 py-3 text-left">
-                    Lessee
-                  </th>
-                  <th scope="col" className="px-3 py-3 text-left">
-                    Start Date
-                  </th>
-                  <th scope="col" className="px-3 py-3 text-left">
-                    End Date
-                  </th>
-                  <th scope="col" className="px-3 py-3 text-left">
-                    Vehicle ID
-                  </th>
-                  <th scope="col" className="px-3 py-3 text-left">
-                    Pay Day
-                  </th>
-                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
-                    <span className="sr-only">Edit</span>
-                  </th>
+                  <th scope="col" className="px-3 py-3 text-left">Status</th>
+                  <th scope="col" className="px-3 py-3 text-left">ID</th>
+                  <th scope="col" className="px-3 py-3 text-left">Envelop ID</th>
+                  <th scope="col" className="px-3 py-3 text-left">Created At</th>
+                  <th scope="col" className="px-3 py-3 text-left">Updated At</th>
+                  <th scope="col" className="px-3 py-3 text-left">Lessee</th>
+                  <th scope="col" className="px-3 py-3 text-left">Start Date</th>
+                  <th scope="col" className="px-3 py-3 text-left">End Date</th>
+                  <th scope="col" className="px-3 py-3 text-left">Vehicle ID</th>
+                  <th scope="col" className="px-3 py-3 text-left">Pay Day</th>
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-200 bg-white">
@@ -111,12 +150,6 @@ function App() {
                     <td className="whitespace-nowrap px-3 py-4 text-gray-500">{person.endLeaseDate}</td>
                     <td className="whitespace-nowrap px-3 py-4 text-gray-500">{person.vehicleId}</td>
                     <td className="whitespace-nowrap px-3 py-4 text-gray-500 text-center">{person.paymentDueDay}</td>
-
-                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                      {/* <a className="text-indigo-600 hover:text-indigo-900">
-                        Edit<span className="sr-only">, {person.name}</span>
-                      </a> */}
-                    </td>
                   </tr>
                 ))}
               </tbody>
@@ -124,7 +157,54 @@ function App() {
           </div>
         </div>
       </div>
+    </div> */
+
+
+    // Second Model
+    <div className="flex flex-row justify-center px-5 py-5 m-5 ">
+      <ul className="divide-y divide-gray-100">
+      {peoples.map((person) => (
+        <li key={person.email} className="relative flex justify-between gap-x-6 py-5">
+          <div className="flex gap-x-4">
+            <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" />
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm font-semibold leading-6 text-gray-900">
+                <a href={person.href}>
+                  <span className="absolute inset-x-0 -top-px bottom-0" />
+                  {person.name}
+                </a>
+              </p>
+              <p className="mt-1 flex text-xs leading-5 text-gray-500">
+                <a href={`mailto:${person.email}`} className="relative truncate hover:underline">
+                  {person.email}
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="flex items-center gap-x-4">
+            <div className="hidden sm:flex sm:flex-col sm:items-end">
+              <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+              {person.lastSeen ? (
+                <p className="mt-1 text-xs leading-5 text-gray-500">
+                  Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
+                </p>
+              ) : (
+                <div className="mt-1 flex items-center gap-x-1.5">
+                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
+                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
+                  </div>
+                  <p className="text-xs leading-5 text-gray-500">Online</p>
+                </div>
+              )}
+            </div>
+            
+          </div>
+        </li>
+      ))}
+    </ul>
     </div>
+    
+  
   );
 }
 
